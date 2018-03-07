5127870a00a4dd90068c70e64b5753f7cee694d6
Derek Chen <derekhound@gmail.com>
2018-03-07 18:06:07 +0800

    Remove Hellocontroller

--- src/main/java/com/company/linebot/controller/ProductController.java
+++ src/main/java/com/company/linebot/controller/ProductController.java
@@ -1,33 +1,50 @@
 package com.company.linebot.controller;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
 
 import com.company.linebot.domain.Product;
 import com.company.linebot.domain.repository.ProductRepository;
+import com.company.linebot.dto.ProductDto;
 
-@Controller
+@RestController
+@RequestMapping("products")
 public class ProductController {
 
+    @Autowired
+    private ProductRepository productRepository;
 
-    @RequestMapping("/products")
-    public String list(Model model) {
-        Product iphone = new Product("P1234", "iPhone 6s", new BigDecimal(500));
-        iphone.setDescription("Apple iPhone 6s smartphone with 4.00-inch 640x1136 display and 8-megapixel rear camera");
-        iphone.setCategory("Smartphone");
-        iphone.setManufacturer("Apple");
-        iphone.setUnitsInStock(1000);
-        model.addAttribute("product", iphone);
-        return "products";
+    @RequestMapping(method = RequestMethod.GET)
+    @ResponseStatus(HttpStatus.OK)
+    public List<Product> list() {
+        return productRepository.getAllProducts();
+    }
+
+            /*
+    @RequestMapping(method = RequestMethod.POST)
+    @ResponseStatus(HttpStatus.CREATED)
+    public void create(@RequestBody ProductDto productDto) {
+    }*/
+
+    @RequestMapping(value = "/{productId}", method = RequestMethod.GET)
+    @ResponseStatus(HttpStatus.OK)
+    public Product read(@PathVariable("productId") String productId) {
+        Product iphone =  new Product("P1234", productId, new BigDecimal(500));
+        return iphone;
     }
 }
