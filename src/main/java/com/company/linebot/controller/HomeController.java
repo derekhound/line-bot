287816f71d760902a5886ccb9645f94a33c283b6
Derek Chen <derekhound@gmail.com>
2018-03-08 16:24:15 +0800

    Support i18n

--- src/main/java/com/company/linebot/controller/HomeController.java
+++ src/main/java/com/company/linebot/controller/HomeController.java
@@ -1,5 +1,9 @@
 package com.company.linebot.controller;
 
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,8 +11,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
 
+  @Autowired
+  MessageSource messageSource;
+
   @RequestMapping("/")
-  public String welcome(Model model) {
+  public String welcome(Locale locale, Model model) {
     model.addAttribute("greeting", "Welcome to Web Store!");
     model.addAttribute("tagline", "The one and only amazing web store");
 
